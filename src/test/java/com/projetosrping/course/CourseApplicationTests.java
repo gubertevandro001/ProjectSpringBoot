a1f0dbb2ee33df864c2380ab689073c1bbf67396
Evandro Gubert <gubertevandro33@gmail.com>
2022-04-04 21:36:31 -0300

    Project created

---
+++ src/test/java/com/projetosrping/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosrping.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
